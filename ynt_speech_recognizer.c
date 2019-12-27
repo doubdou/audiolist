5ba5a0eb6a3c2d256f7294198a028c7471cbede9
jinzw <jinzw@ynt.ai>
2019-12-27 11:29:59 +0800

    recognizer优化， 追加model_id和normalize参数设置的接口等

--- ynt_speech_recognizer.c
+++ ynt_speech_recognizer.c
@@ -7,12 +7,36 @@
 #undef YNT_DEBUG_AUDIO_DATA
 
 
-ynt_speech_recognizer_t* ynt_speech_recognizer_create(ynt_speech_recognizer_config_t* asr_conf)
+int ynt_speech_recognizer_model_id_set(ynt_speech_recognizer_t* recognizer, unsigned int model_id)
+{
+    if(NULL == recognizer)
+    {
+        return -1;
+    }
+	recognizer->model_id = model_id;
+
+	return 0;
+}
+
+int ynt_speech_recognizer_normalize_set(ynt_speech_recognizer_t* recognizer, unsigned int normalize)
+{
+    if(NULL == recognizer)
+    {
+        return -1;
+    }
+	recognizer->normalize = normalize;
+
+	return 0;
+}
+
+
+ynt_speech_recognizer_t* ynt_speech_recognizer_create(const void* asr_client, ynt_speech_recognizer_config_t* asr_conf)
 {
    ynt_speech_recognizer_t* recognizer = (ynt_speech_recognizer_t*)malloc(sizeof(ynt_speech_recognizer_t));
    memset(recognizer, 0x0, sizeof(ynt_speech_recognizer_t));
 
    recognizer->hotword = asr_conf->hotword;
+   recognizer->asr_client = asr_client;
 
    return recognizer;
 }
@@ -26,12 +50,13 @@ void ynt_speech_recognizer_destroy(ynt_speech_recognizer_t* recognizer)
     }
 }
 
-char* ynt_asr_process(ynt_audio_ctl_t* audio_ctl, ynt_speech_recognizer_t* recognizer)
-{	
-    char* rslt = NULL;
-	char* pcm_addr = NULL;
-	unsigned int pcm_len = 0;
 
+int ynt_asr_process(ynt_audio_ctl_t* audio_ctl, ynt_speech_recognizer_t* recognizer)
+{	
+    char* rslt              = NULL;
+	unsigned char* pcm_addr = NULL;
+	unsigned int pcm_len    = 0;
+    int err_code            = 0;
 
 #ifdef YNT_DEBUG_AUDIO_DATA
 	char filename[1024];
@@ -41,30 +66,17 @@ char* ynt_asr_process(ynt_audio_ctl_t* audio_ctl, ynt_speech_recognizer_t* recog
 	struct tm * lt;
 	time (&t);
 	lt = localtime (&t);
-	sprintf(filename, "/tmp/ynt_asr_process_%p_%d%d%d%d%d%d.pcm", (void*)audio_ctl, 
+	sprintf(filename, "/tmp/ynt_asr_process_%p_%04d%02d%02d%02d%02d%02d.pcm", (void*)audio_ctl, 
 				lt->tm_year+1900, lt->tm_mon, lt->tm_mday, lt->tm_hour, lt->tm_min, lt->tm_sec);
 #endif
 
 	pcm_addr = ynt_audiolist_merge_memory(audio_ctl);
 	if(pcm_addr == NULL){
 		ynt_audiolist_destroy(audio_ctl);
-		return NULL;
+		return -1;
 	}
 	
 	pcm_len = audio_ctl->node_count * VAD_NODE_SIZE;
-
-    rslt = ynt_asr_client_speech_recognizer(1, 8000, 16, 1, recognizer->model_id, 0, 0, NULL, pcm_addr, pcm_len);
-	//asr结果回调函数
-	recognizer->result_func(rslt, recognizer->user_data);
-	//if (NULL != rslt)
-	//{
-	//	free(rslt);
-	//}
-	ynt_audiolist_free_memory(pcm_addr);
-
-	/* 销毁该链表,生成新链表 */
-	ynt_audiolist_destroy(audio_ctl);
-	audio_ctl = ynt_audiolist_create();
 
 #ifdef YNT_DEBUG_AUDIO_DATA
 	/* file to write to */
@@ -75,7 +87,7 @@ char* ynt_asr_process(ynt_audio_ctl_t* audio_ctl, ynt_speech_recognizer_t* recog
 	} 
 	else 
 	{
-	    write_size = fwrite(pcm_addr, 1, pcm_len, audio_out);
+		write_size = fwrite(pcm_addr, 1, pcm_len, audio_out);
 		fclose(audio_out);
 		if(write_size != pcm_len) 
 		{
@@ -83,8 +95,37 @@ char* ynt_asr_process(ynt_audio_ctl_t* audio_ctl, ynt_speech_recognizer_t* recog
 		}
 	}
 #endif
+    err_code = ynt_asr_client_speech_recognizer(recognizer->asr_client,
+		                                    1, 
+		                                    8000, 
+		                                    16, 
+		                                    recognizer->model_id, 
+		                                    0, 
+		                                    recognizer->hotword, 
+		                                    pcm_addr, 
+		                                    pcm_len, 
+		                                    recognizer->normalize,
+		                                    &rslt);
+    if(err_code != 0)
+    {
+       	printf("ynt asr recognize error(error code: %d)\n", err_code);
+		return -2;
+    }else {
+		//asr结果回调函数
+		recognizer->result_func(rslt, recognizer->user_data);
+    }
+   
+	if (NULL != rslt)
+	{
+		free(rslt);
+	}
+	ynt_audiolist_free_memory(pcm_addr);
 
-    return rslt;
+	/* 销毁该链表,生成新链表 */
+	ynt_audiolist_destroy(audio_ctl);
+	audio_ctl = ynt_audiolist_create();
+
+    return 0;
 }
 
 
--- ynt_speech_recognizer.h
+++ ynt_speech_recognizer.h
@@ -23,18 +23,24 @@ typedef void (*asr_result_callback)(const char*, void*);
 
 typedef struct {
     char* id;
+	const void* asr_client;
     unsigned int model_id;
+	unsigned int normalize;
 	char* hotword;
 	char* result;
 	asr_result_callback result_func;
 	void* user_data;
 }ynt_speech_recognizer_t;
 
-ynt_speech_recognizer_t* ynt_speech_recognizer_create(ynt_speech_recognizer_config_t* asr_conf);
+int ynt_speech_recognizer_model_id_set(ynt_speech_recognizer_t* recognizer, unsigned int model_id);
+
+int ynt_speech_recognizer_normalize_set(ynt_speech_recognizer_t* recognizer, unsigned int normalize);
+
+ynt_speech_recognizer_t* ynt_speech_recognizer_create(const void* asr_client, ynt_speech_recognizer_config_t* asr_conf);
 
 void ynt_speech_recognizer_destroy(ynt_speech_recognizer_t* recognizer);
 
-char* ynt_asr_process(ynt_audio_ctl_t* audio_ctl, ynt_speech_recognizer_t* recognizer);
+int ynt_asr_process(ynt_audio_ctl_t* audio_ctl, ynt_speech_recognizer_t* recognizer);
 
 
 #endif
